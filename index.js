0a25527642c8ef4edcea7e66cb95db93e66e9c1b
nmtuyen316 <95425898+nmtuyen316@users.noreply.github.com>
2022-07-23 19:49:16 +0700

    pug

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const app = express();
 const pug = require('pug');
 
 const PORT = process.env.PORT || 3000;
+pug.compileFile(`${__dirname}/src/views/index.pug`);
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/src/views/`);
 
 app.use("/public/css", express.static(`${__dirname}/public/css`));
 app.use("/public/script", express.static(`${__dirname}/public/script`));
 
 app.get('/',function(req,res){
-  res.sendFile(`${__dirname}/src/views/index.html`);
+  // res.sendFile(`${__dirname}/src/views/index.html`);
+  res.render('index');
 });
 
 app.listen(PORT, ()=>{
